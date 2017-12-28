ecc9854b339c47aa2b80da51a375e35b646d5ce5
Sandy Chuchuca <sandy.chuchuca@gmail.com>
2017-12-28 17:19:19 +0000

    Refactored router

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const authentication = require('./server/routes/middlewares/authentication');
+
+
+// middleware that authenticates users
+app.use(authentication);
 
 // routes
-var index = require('./server/routes/index');
+const index = require('./server/routes/index');
+const users = require('./server/routes/users/user');
 
 app.use('/', index);
+app.use('/users', users);
 
-app.listen(4030, function () {
+app.listen(4030, () => {
   console.log('Listening on port 4030!')
-})
+});
